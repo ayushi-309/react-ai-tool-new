ceff71f1d3859141a4e95d626c3d885dfead4053
ayushi-309 <tayushi751@gmail.com>
2025-08-19 00:01:25 +0530

    ai

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
 import './App.css'
 
+
 function App() {
 
   return (
-    <>
-      <h1 className='bg-cyan-600 text-5xl'>React AI tool</h1>
-    </>
+    <div className='grid grid-cols-5 h-screen'>
+      <div className='col-span-1 bg-zinc-800'>
+       hello
+      </div>
+      <div className='col-span-4'>
+        <div className='container h-110'>
+
+        </div>
+        <div className='bg-zinc-800 w-1/2'>
+            <input type = "text" placeholder='Ask me anything' />
+            <button>Ask</button>
+        </div>
+
+      </div>
+    </div>
   )
 }
 
